b676331b70067ea5f6ff5babd4ed240b2848ab83
kaizer1v <kaizer1v@gmail.com>
2020-07-25 06:47:58 +0530

    single pages

--- panoptic_flask.py
+++ panoptic_flask.py
@@ -14,9 +14,29 @@ def get_frts(state):
     return state
 
 
+@app.route('/submit_frt')
+def submit_frt():
+    return render_template('submit_frt.html')
+
+
+@app.route('/case_studies')
+def case_studies():
+    return render_template('case_studies.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
