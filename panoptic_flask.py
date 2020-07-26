4151a754e355fd4297aafa1106b0b5f66494963a
kaizer1v <kaizer1v@gmail.com>
2020-07-26 15:14:47 +0530

    main file for running flask

---
+++ panoptic_flask.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+from folium_map import generate_map
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    # TODO: Srihari to generate iframe for map
+    generated_iframe = generate_map()
+    return render_template('home.html', iframe_map=generated_iframe)
+
+@app.route('/map')
+def map():
+    return render_template('map_plot.html')
+
+@app.route('/state/<state>')
+def get_frts(state):
+    return state
+
+
+@app.route('/submit_frt')
+def submit_frt():
+    return render_template('submit_frt.html')
+
+
+@app.route('/case_studies')
+def case_studies():
+    return render_template('case_studies.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run()
