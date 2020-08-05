ec4455b64ee152468a998e9772bcdaa69075c82e
kaizer1v <kaizer1v@gmail.com>
2020-08-06 00:48:34 +0530

    moved all DB operations into a single file

--- model/model.py
+++ model/model.py
@@ -1,6 +1,34 @@
-"""Model file for accessing the base functions"""
-# from model import utils
-from model.db_helper import execute_select_query
+import os
+from json import load
+from mysql import connector
+
+def execute_select_query(query:str):
+    """
+    Executes the SELECT query on MYSQL database
+
+    Arguments:
+        query {str} -- Query to execute
+    Returns:
+        {list} -- Columns returned by the query
+        {list} -- List of data returned by the query
+    """
+
+    conn = connector.connect (
+        host = os.environ.get('MYSQL_HOST'),
+        user = os.environ.get('MYSQL_USERNAME'),
+        password = os.environ.get('MYSQL_PASSWORD')
+    )
+
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+    # Get all the columns of data
+    headers = [x[0] for x in cursor.description]
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return headers, data
 
 def get_state_frt(state:str):
     """
