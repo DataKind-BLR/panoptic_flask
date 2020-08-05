12ccda5b116d770d015830a7765338994ee59a1d
kaizer1v <kaizer1v@gmail.com>
2020-08-06 00:28:28 +0530

    given a geojson shape file + data, generate_map will return a folium chloropeth html iframe object

--- folium_map.py
+++ folium_map.py
@@ -1,64 +1,9 @@
-import geopandas as gpd
-import pandas as pd
 import folium
 import branca
-import requests
-import json
-from folium import IFrame
 from folium.features import GeoJson, GeoJsonTooltip, GeoJsonPopup
 
-with open('./shape_files/india_gdf.json') as response:
-    india_gdf = json.load(response)
 
-gdf = gpd.read_file('./shape_files/states_india.shp')
-
-gdf['Count of FRT Systems'] = 0
-gdf['FRT Systems Deployed'] = 'None'
-gdf['Authority'] = 'None'
-gdf['Place'] = 'None'
-gdf['Purpose'] = 'None'
-
-gdf['FRT Systems Deployed'][0] = 'TSCOP + CCTNS'
-gdf['Count of FRT Systems'][0] = 1
-gdf['Authority'][0] = 'Hyderabad Police'
-gdf['Place'][0] = 'Hyderabad'
-gdf['Purpose'][0] = 'Security/ Surveillance'
-
-gdf['FRT Systems Deployed'][35] = 'AFRS'
-gdf['Count of FRT Systems'][35] = 1
-gdf['Authority'][35] = 'Delhi Police'
-gdf['Place'][35] = 'Delhi'
-gdf['Purpose'][35] = 'Security/ Surveillance'
-
-gdf['FRT Systems Deployed'][27] = 'Trinetra'
-gdf['Count of FRT Systems'][27] = 1
-gdf['Authority'][27] = 'UP Police'
-gdf['Place'][27] = 'UP/Lucknow'
-gdf['Purpose'][27] = 'Security/ Surveillance'
-
-gdf['FRT Systems Deployed'][25] = 'FaceTagr'
-gdf['Count of FRT Systems'][25] = 1
-gdf['Authority'][25] = 'Chennai Police'
-gdf['Place'][25] = 'Chennai'
-gdf['Purpose'][25] = 'Security/ Surveillance'
-
-gdf['FRT Systems Deployed'][22] = 'Punjab Artificial Intelligence System'
-gdf['Count of FRT Systems'][22] = 1
-gdf['Authority'][22] = 'Punjab Police'
-gdf['Place'][22] = 'Punjab'
-gdf['Purpose'][22] = 'Security/ Surveillance'
-
-gdf['FRT Systems Deployed'][14] = 'South Western Railways'
-gdf['Count of FRT Systems'][14] = 1
-gdf['Authority'][14] = 'South Western Railways'
-gdf['Place'][14] = 'KSR Bengaluru Station'
-gdf['Purpose'][14] = 'Security/ Surveillance'
-
-
-def generate_map():
-
-    #gdf.to_file("./shape_files/india_gdf.json", driver="GeoJSON")
-
+def generate_map(geojson, data):
     # Create a white image of 4 pixels, and embed it in a url.
     white_tile = branca.utilities.image_to_url([[1, 1], [1, 1]])
 
@@ -105,8 +50,8 @@ def generate_map():
     )
 
     g = folium.Choropleth(
-        geo_data=india_gdf,
-        data=gdf,
+        geo_data=geojson,
+        data=data,
         columns=['st_nm', 'Count of FRT Systems'],
         key_on='properties.st_nm',
         fill_color='Set3',
@@ -120,9 +65,9 @@ def generate_map():
         if key.startswith('color_map'):
             del(g._children[key])
 
-    for i in range(len(india_gdf['features'])):
+    for i in range(len(geojson['features'])):
         gs = folium.GeoJson(
-            india_gdf['features'][i],
+            geojson['features'][i],
             style_function=lambda feature: {
                 'fillColor': '#ffff00',
                 'color': 'black',
@@ -131,15 +76,15 @@ def generate_map():
             }
         )
 
-        state = india_gdf['features'][i]['properties']['st_nm']
-        auth = india_gdf['features'][i]['properties']['Authority']
+        state = geojson['features'][i]['properties']['st_nm']
+        auth = geojson['features'][i]['properties']['Authority']
         popup_html = '''
             <h1>Name: </h1>{}<br />\
             <h2>Location: </h2>{}<br />\
             <a href="about:blank">More Details</a>
         '''.format(state, auth)
         
-        popup = IFrame(popup_html, width=200, height=150)
+        popup = folium.IFrame(popup_html, width=200, height=150)
         folium.Popup(popup, max_width=300).add_to(gs)
         gs.add_to(g)
 
--- panoptic_flask.py
+++ panoptic_flask.py
@@ -1,17 +1,61 @@
+import json
+import geopandas as gpd
 from flask import Flask, render_template
 from folium_map import generate_map
 app = Flask(__name__)
 
+with open('./shape_files/india_gdf.json') as response:
+    india_gdf = json.load(response)
+
+gdf = gpd.read_file('./shape_files/states_india.shp')
+gdf['Count of FRT Systems'] = 0
+gdf['FRT Systems Deployed'] = 'None'
+gdf['Authority'] = 'None'
+gdf['Place'] = 'None'
+gdf['Purpose'] = 'None'
+
+gdf['FRT Systems Deployed'][0] = 'TSCOP + CCTNS'
+gdf['Count of FRT Systems'][0] = 1
+gdf['Authority'][0] = 'Hyderabad Police'
+gdf['Place'][0] = 'Hyderabad'
+gdf['Purpose'][0] = 'Security/ Surveillance'
+
+gdf['FRT Systems Deployed'][35] = 'AFRS'
+gdf['Count of FRT Systems'][35] = 1
+gdf['Authority'][35] = 'Delhi Police'
+gdf['Place'][35] = 'Delhi'
+gdf['Purpose'][35] = 'Security/ Surveillance'
+
+gdf['FRT Systems Deployed'][27] = 'Trinetra'
+gdf['Count of FRT Systems'][27] = 1
+gdf['Authority'][27] = 'UP Police'
+gdf['Place'][27] = 'UP/Lucknow'
+gdf['Purpose'][27] = 'Security/ Surveillance'
+
+gdf['FRT Systems Deployed'][25] = 'FaceTagr'
+gdf['Count of FRT Systems'][25] = 1
+gdf['Authority'][25] = 'Chennai Police'
+gdf['Place'][25] = 'Chennai'
+gdf['Purpose'][25] = 'Security/ Surveillance'
+
+gdf['FRT Systems Deployed'][22] = 'Punjab Artificial Intelligence System'
+gdf['Count of FRT Systems'][22] = 1
+gdf['Authority'][22] = 'Punjab Police'
+gdf['Place'][22] = 'Punjab'
+gdf['Purpose'][22] = 'Security/ Surveillance'
+
+gdf['FRT Systems Deployed'][14] = 'South Western Railways'
+gdf['Count of FRT Systems'][14] = 1
+gdf['Authority'][14] = 'South Western Railways'
+gdf['Place'][14] = 'KSR Bengaluru Station'
+gdf['Purpose'][14] = 'Security/ Surveillance'
+
 
 @app.route('/')
 def root():
-    # TODO: Srihari to generate iframe for map
-    html_map = generate_map()
+    html_map = generate_map(geojson=india_gdf, data=gdf)
     return render_template('home.html', iframe=html_map)
 
-@app.route('/map')
-def map():
-    return render_template('map_plot.html')
 
 @app.route('/state/<state>')
 def get_frts(state):
